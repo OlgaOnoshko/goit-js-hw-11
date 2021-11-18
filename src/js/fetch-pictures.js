a2b9671bb72a3831047493a09b34a4dbda29b364
OlgaOnoshko <onoshko.olga@gmail.com>
2021-11-18 15:20:55 +0200

    styles, async and lightbox added

--- src/js/fetch-pictures.js
+++ src/js/fetch-pictures.js
@@ -10,39 +10,35 @@ export default class FetchPicturesService {
         this.picturesNumber = 0;
     }
 
-    fetchPictures() {
-        return fetch(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=4`)
-            .then(responce => responce.json())
-            .then(({ hits, totalHits }) => {
-                this.page += 1;
-                console.log(totalHits);
-                this.picturesNumber += hits
-                if (hits.length !== 0) {
-                    Notify.info(`Hooray! We found ${totalHits} images!`)
-                }
-                return hits;
-            })
-    }
-    
-    // picturesNumberCount() {
-    //     if (this.picturesNumber === totalHits) {
-    //         Notify.info("We're sorry, but you've reached the end of search results.");
-    //         refs.loadMoreBtn.classList.add('is-hidden');
-    //     }
+    // fetchPictures() {
+    //     return fetch(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+    //         .then(responce => responce.json())
+    //         .then(data => {
+    //             this.page += 1;
+    //             return data;
+                
+    //       })
     // }
 
-    pageReset() {
+    fetchPictures = async () => {
+        const responce = await fetch(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`);
+        const data = await responce.json();
+        this.page += 1;
+        return data;
+    };
+    
+        pageReset() {
         this.page = 1;
-    }
+    };
 
     get query() {
         return this.searchQuery;
-    }
+    };
 
     set query(newQuery) {
         this.searchQuery = newQuery;
-    }
-}
+    };
+};
 
 
     
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import '../sass/main.scss';
-// import { fetchPictures } from './fetch-pictures';
 import FetchPicturesService from './fetch-pictures';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
@@ -11,7 +11,7 @@ const refs = {
     loadMoreBtn: document.querySelector('.load-more'),
     gallery: document.querySelector('.gallery'),
 }
-const fetchPicturesService = new FetchPicturesService
+const fetchPicturesService = new (FetchPicturesService);
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', loadMorePics);
@@ -19,46 +19,64 @@ refs.loadMoreBtn.classList.add('is-hidden');
 
 function onSearch(evt) {
     evt.preventDefault();
+    let picturesNumber = 0;
 
     removePicturesMarkUp();
     fetchPicturesService.query = evt.currentTarget.elements.searchQuery.value;
     fetchPicturesService.pageReset();
     fetchPicturesService.fetchPictures()
-      .then(hits => {
-        if (hits.length === 0) {
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-        }
-          addPicturesMarkUp(hits);
-          refs.loadMoreBtn.classList.remove('is-hidden');
-    } 
-)}
+        .then(({ hits, totalHits }) => {
+            if (hits.length === 0) {
+                Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+                return;
+            }
+            if (hits.length < 40) {
+                Notify.failure("We're sorry, but you've reached the end of search results.")
+                addPicturesMarkUp(hits);
+                return;
+            }
+            if (hits.length !== 0) {
+                Notify.info(`Hooray! We found ${totalHits} images!`)
+            }
+            addPicturesMarkUp(hits);
+            refs.loadMoreBtn.classList.remove('is-hidden');
+        });
+};
     
-
 function loadMorePics(evt) {
     fetchPicturesService.fetchPictures()
-        .then(hits => addPicturesMarkUp(hits));
-}
+        .then(({ hits, totalHits }) => {
+            // console.log(hits.length);
+            if (hits.length < 40) {
+                refs.loadMoreBtn.classList.add('is-hidden');
+                Notify.failure("We're sorry, but you've reached the end of search results.")
+                addPicturesMarkUp(hits);
+                return;
+            }
+            addPicturesMarkUp(hits);
+        });
+};
 
 function addPicturesMarkUp(pictures) {
     const markUp = pictures.map(picture => {
         const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = picture;
-        return `<a class="gallery__link" href="${largeImageURL}">
+        return `<a class="gallery-link" href="${largeImageURL}">
       <div class="photo-card">
           <div class="photo-thumb">
             <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy"/>
           </div>
           <div class="info">
             <p class="info-item">
-              <b>Likes</b>${likes}
+              <b>Likes</b><span>${likes}</span>
               </p>
             <p class="info-item">
-              <b>Views</b>${views}
+              <b>Views</b><span>${views}</span>
               </p>
             <p class="info-item">
-              <b>Comments</b>${comments}
+              <b>Comments</b><span>${comments}</span>
               </p>
             <p class="info-item">
-              <b>Downloads</b>${downloads}
+              <b>Downloads</b><span>${downloads}</span>
               </p>
           </div>
       </div>
@@ -66,10 +84,13 @@ function addPicturesMarkUp(pictures) {
     })
         .join('');
     
+    
     refs.gallery.innerHTML += markUp;
-}
+    const gallery = new SimpleLightbox('.gallery a');
+    gallery.refresh();
+};
 
 function removePicturesMarkUp() {
     refs.gallery.innerHTML = '';
-}
+};
 
